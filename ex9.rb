439dc580dddfbfcaf9ec3bfc18dfa9dbfc6946f0
FrauBienenstich <susanne.dewein@gmail.com>
2014-01-04 18:50:14 +0100

    Put code into a class
    and some other adjustments...

--- ex9.rb
+++ ex9.rb
@@ -4,22 +4,58 @@
 # be printed. I counts only as one try if the user inputs the same number consecutively.
 
 
-@secret_number = 34
-puts "Please guess my secret number."
-@guess = gets.chomp.to_i
 
-def hint
-	if @guess < @secret_number
-		puts "Too small! Please try again:"
-		@guess = gets.chomp.to_i
-	elsif @guess > @secret_number
-		puts "Too big! Please try again: "
-		@guess = gets.chomp.to_i
-	end
+# @secret_number = 34
+# puts "Please guess my secret number."
+# @guess = gets.chomp.to_i
+
+# def hint
+# 	if @guess < @secret_number
+# 		puts "Too small! Please try again:"
+# 		@guess = gets.chomp.to_i
+# 	elsif @guess > @secret_number
+# 		puts "Too big! Please try again: "
+# 		@guess = gets.chomp.to_i
+# 	end
+# end
+
+# while @guess != @secret_number
+# 	hint
+# end
+
+# puts "Correct!" if @guess == @secret_number
+
+
+class GuessMe
+
+  puts "Please guess my number."
+
+  def initialize(secret_number)
+    @secret_number = secret_number
+    user_input 
+  end
+
+  def user_input
+    @guess = gets.chomp.to_i
+  end
+
+  def hint
+    if @guess < @secret_number
+      puts "Too small! Please try again:"
+     user_input
+    elsif @guess > @secret_number
+      puts "Too big! Please try again: "
+      user_input
+    end
+  end
+
+  def start
+    while @guess != @secret_number
+      hint
+    end
+    puts "Correct!" if @guess == @secret_number
+  end 
 end
 
-while @guess != @secret_number
-	hint
-end
-
-puts "Correct!" if @guess == @secret_number
+guessing_game = GuessMe.new(34)
+guessing_game.start
