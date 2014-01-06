6a9fd848a77ec234dcf011ab29534c58e9190c96
FrauBienenstich <susanne.dewein@gmail.com>
2014-01-06 20:30:33 +0100

    Reverse a list!

---
+++ ex2.2.rb
@@ -0,0 +1,15 @@
+# Write function that reverses a list, preferably in place.
+
+list = [ 5, 1, 455, 2, 8, 0]
+
+
+def reverse(list)
+  reversed_list = []
+  list.each do |n|
+    reversed_list.unshift(n)
+  end
+  return reversed_list 
+end
+
+puts reverse(list)
+
