64fad9da47c6a78d9d6c320e342d89f728159203
Susanne Dewein <susanne.dewein@gmail.com>
2014-02-12 11:49:35 +0100

    Concatenate list

---
+++ ex2.9.rb
@@ -0,0 +1,12 @@
+# Write a function that concatenates two lists.
+
+list_1 = ["Hund", "Katze", "Maus"]
+list_2 = [1, 2, 3]
+
+def concatenate(list1, list2)
+  full_list = list1 + list2
+end
+
+puts concatenate(list_1, list_2)
+
+
