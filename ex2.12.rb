d75a636cfbc4ccfc9c01fec276daacd2d1012aa5
Susanne Dewein <susanne.dewein@gmail.com>
2014-02-14 12:14:43 +0100

    Optimize code through memoization
    Soooo much faster now!

--- ex2.12.rb
+++ ex2.12.rb
@@ -1,18 +1,23 @@
 #Write a function that computes the list of the first 100 Fibonacci numbers.
 
-def fibonacci(n)
-  return n if n <= 1
+# def fibonacci(n)
+#   return n if n <= 1
 
-  fibonacci(n-1) + fibonacci(n-2)
+#   fibonacci(n-1) + fibonacci(n-2)
+# end
+
+FIBONACCI_CACHE = {}
+
+def memoized_fibonacci(n)
+  FIBONACCI_CACHE[n] ||= (n <= 1 ? n : memoized_fibonacci(n-1) + memoized_fibonacci(n-2)) # fibonacci wird für jede zahl gespeichert
 end
 
 def print_list
   n = 0..99
   n.each do |fib|
     puts "This is the number: #{fib}"
-    puts fibonacci(fib)
+    puts memoized_fibonacci(fib)
   end  
 end
 
 print_list
-# this takes ages! computer still stuck at 41 after 2 minutes
